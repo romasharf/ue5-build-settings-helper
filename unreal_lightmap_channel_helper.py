a62381f32a707a33509d678be4ca7d47a8d4af8e
Roman Sharafetdinov <63375261+romasharf@users.noreply.github.com>
2023-11-03 21:56:33 +0100

    Update unreal_lightmap_channel_helper.py

--- unreal_lightmap_channel_helper.py
+++ unreal_lightmap_channel_helper.py
@@ -25,12 +25,14 @@ def setNiceLightmapChannels():
         number_of_lod = 0 #number of current lod to check
         for number_of_lod in range(lods_number):
             
+            my_lod = sm_edit_subsys.get_lod_build_settings(selectedAsset, number_of_lod) #lod of the current asset which we need to check for bad lightmap build settings
+            
             if (sm_edit_subsys.get_lod_build_settings(selectedAsset, number_of_lod).src_lightmap_index != 1) or (sm_edit_subsys.get_lod_build_settings(selectedAsset, number_of_lod).dst_lightmap_index != 1) or (sm_edit_subsys.get_lod_build_settings(selectedAsset, number_of_lod).generate_lightmap_u_vs == False):
                 print('Wrong Lightmap build settings: '+str(selectedAsset))
                 print('LOD: '+str(number_of_lod))
-                print('Generate Lightmap UVs: '+str(sm_edit_subsys.get_lod_build_settings(selectedAsset, number_of_lod).generate_lightmap_u_vs))
-                print("source lightmap channel: " + str(sm_edit_subsys.get_lod_build_settings(selectedAsset, number_of_lod).src_lightmap_index)) #get lod0 build settings
-                print("destination lightmap channel: " + str(sm_edit_subsys.get_lod_build_settings(selectedAsset, number_of_lod).dst_lightmap_index)+'\n ') #get lod0 build settings
+                print('Generate Lightmap UVs: '+str(my_lod.generate_lightmap_u_vs)) #show if the lod use auto UVs
+                print("source lightmap channel: " + str(my_lod.src_lightmap_index)) #show the lod's source lightmap
+                print("destination lightmap channel: " + str(my_lod.dst_lightmap_index)+'\n ') #show the lod's destination lightmap
                 print(' \n ')
                 wrong_lm_settings_count = wrong_lm_settings_count + 1
             
